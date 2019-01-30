d692b9254cde85d5e0e1af03f5b49f3b319fbdd7
sjoerdjell <sjoerdjell@ecs.vuw.ac.nz>
2019-01-30 15:41:30 +1300

    Added 'home' to installed apps in settings.py

--- rentwiz/settings.py
+++ rentwiz/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
     'UserManagement.apps.UsersConfig',
 ]
 
@@ -55,8 +56,7 @@ ROOT_URLCONF = 'rentwiz.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'home/templates'),
-                 os.path.join(BASE_DIR, 'UserManagement/templates'),],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
