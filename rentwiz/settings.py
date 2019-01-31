0d3657fe43449d563f1be3a66df4d7253166ad40
hayleymapley <hmapley@gmail.com>
2019-01-31 15:39:20 +1300

    Removed psycopg2 reference

--- rentwiz/settings.py
+++ rentwiz/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'rentwiz.wsgi.application'
 if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
