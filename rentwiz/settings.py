8a0ba9ebd8407d5b4e796c92e8be3d428a437cbb
hayleymapley <hmapley@gmail.com>
2019-01-31 13:27:58 +1300

    Added static root

--- rentwiz/settings.py
+++ rentwiz/settings.py
@@ -125,6 +125,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
