7ed75f6af0cc772bfe0094bd1e72f29c8edf59ed
hayleymapley <hmapley@gmail.com>
2019-02-04 11:35:15 +1300

    Added postgresql local db

--- rentwiz/settings.py
+++ rentwiz/settings.py
@@ -90,12 +90,24 @@ WSGI_APPLICATION = 'rentwiz.wsgi.application'
 #         }
 #     }
 # else:
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'practicedb',
+        'USER': 'postgres',
+        'PASSWORD': 'pass123',
+        'HOST': 'localhost',
+        'PORT': '5432',
         }
     }
+
 
 AUTH_USER_MODEL = 'UserManagement.RegisteredUser'
 
