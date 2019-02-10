a4b5c1a79d0bf16b44e52b5d4e3fa597ef142232
hayleymapley <hmapley@gmail.com>
2019-02-11 09:45:48 +1300

    Fixed indentation in urls

--- tenant_application/urls.py
+++ tenant_application/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('apply/', views.apply, name='apply'),
-      path('addTenants/', views.addTenants, name='addTenants'),
-      path('conditions/', views.conditions, name='conditions'),
-            path('confirmation/', views.confirmation, name='confirmation')
-
+    path('addTenants/', views.addTenants, name='addTenants'),
+    path('conditions/', views.conditions, name='conditions'),
+    path('confirmation/', views.confirmation, name='confirmation')
 ]
