f3f70bbeaae2b7dcbe49090fdce009a55474e6e2
hayleymapley <hmapley@gmail.com>
2019-02-11 22:12:35 +1300

    Removed admin and models stuff

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,1 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from .models import Property #this line added
-admin.site.register(Property)#this line added
