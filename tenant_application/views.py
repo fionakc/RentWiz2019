41e85ac58f362992674034c3e0776dec62a35e74
hayleymapley <hmapley@gmail.com>
2019-02-11 21:12:16 +1300

    Added DB info to apply page

--- tenant_application/views.py
+++ tenant_application/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import render
 from property_listing.models import Property;
 
-# Create your views here.
+
 def apply(request, id):
     qset = Property.objects.get(id=id)
     context = {'queryset': qset,
                }
     return render(request, 'apply.html', context)
 
+
 def addTenants(request):
     return render(request, 'addTenants.html')
+
 
 def addMoreT(request):
     return render(request, 'addMoreT.html')
 
+
 def conditions(request):
     return render(request, 'conditions.html')
 
+
 def confirmation(request):
     return render(request, 'confirmation.html')
+
