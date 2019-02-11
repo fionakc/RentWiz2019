97912d351632da8c0f38b70b469706ac23156de0
hayleymapley <hmapley@gmail.com>
2019-02-11 15:56:31 +1300

    Linked test listings page to apply page with ids

--- tenant_application/urls.py
+++ tenant_application/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 
 from . import views
 urlpatterns=[
-    path('apply/', views.apply, name='apply'),
-      path('addTenants/', views.addTenants, name='addTenants'),
-      path('conditions/', views.conditions, name='conditions'),
-            path('confirmation/', views.confirmation, name='confirmation'),
-               path('addMoreT/', views.addMoreT, name='addMoreT')
+    path('apply/<int:id>', views.apply, name='apply'),
+    path('addTenants/', views.addTenants, name='addTenants'),
+    path('conditions/', views.conditions, name='conditions'),
+    path('confirmation/', views.confirmation, name='confirmation'),
+    path('addMoreT/', views.addMoreT, name='addMoreT'),
 
 ]
