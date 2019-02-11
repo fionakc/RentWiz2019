ebbbdd10abcf14eeb2d21ea8db24c8ea18c821e7
LouiseYau <43237563+LouiseYau@users.noreply.github.com>
2019-02-11 14:57:49 +1300

    test

--- tenant_application/urls.py
+++ tenant_application/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('apply/', views.apply, name='apply'),
-      path('addTenants/', views.addTenants, name='addTenants'),
-      path('conditions/', views.conditions, name='conditions'),
-            path('confirmation/', views.confirmation, name='confirmation'),
-               path('addMoreT/', views.addMoreT, name='addMoreT')
+    path('addTenants/', views.addTenants, name='addTenants'),
+    path('conditions/', views.conditions, name='conditions'),
+    path('confirmation/', views.confirmation, name='confirmation'),
+    path('addMoreT/', views.addMoreT, name='addMoreT')
 
 ]
