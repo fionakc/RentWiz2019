51e926ff6c1e1214fab4cdca9c27d41279a1b515
hayleymapley <hmapley@gmail.com>
2019-02-11 22:07:33 +1300

    Added to admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here 2 Carlos and Ali.
+# Register your models here.
+from django.contrib import admin
+from .models import Property #this line added
+admin.site.register(Property)#this line added
