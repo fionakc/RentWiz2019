01e20dcd1f91766c4d22d105b780e350e9eede52
Callum Morris <cmorrisnz101@gmail.com>
2019-02-12 15:10:46 +1300

    Uncommented/commented out troublesome links.

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('search_listings/', views.search_listings, name='search_listings'),
-    path('property_details/<int:id>', views.show_property_details, name='show_property_details'),
+    #path('search_listings/', views.search_listings, name='search_listings'),
+    #path('property_details/<int:id>', views.show_property_details, name='show_property_details'),
 ]
