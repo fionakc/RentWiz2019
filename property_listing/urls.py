89b755a5eec1f3e2b7b638651249361fb1a00c38
sjoerdjell <sjoerdjell@ecs.vuw.ac.nz>
2019-02-12 13:44:16 +1300

    Entered Jelle's edits

--- property_listing/urls.py
+++ property_listing/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('create', views.create, name='create'),
+    path('landlord_profile/<int:landlord_id>', views.landlord_profile, name='landlord_profile'),
 ]
--- property_listing/views.py
+++ property_listing/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from .forms import PostForm
+from .forms import PostForm, LandlordProfileForm, PropertyManagerProfileForm
+from .models import Landlord, Listing, Property, PropertyManager
+from UserManagement.models import Contact
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
+# def index(request):
+#     return render(request, 'index.html')
 
 def create(request):
     if request.method == "POST":
@@ -14,3 +16,79 @@ def create(request):
         form = PostForm()
     return render(request, 'create_listing.html', {'form': form})
 
+
+def landlord_profile(request, landlord_id):
+    landlord = Landlord.objects.get(id=landlord_id)
+    contact = Contact.objects.get(landlord=landlord)
+    initial_landlord_data = {'name': contact.name,
+                    'email': contact.email,
+                    'address_line1': contact.address_line1,
+                    'address_line2': contact.address_line2,
+                    'address_line3': contact.address_line3,
+                    'home_phone': contact.home_phone,
+                    'work_phone': contact.work_phone,
+                    'mobile_phone': contact.mobile_phone,
+                    'tenancy_services_id': landlord.tenancy_services_id,
+                    'is_first_timer': landlord.is_first_timer
+                    }
+
+    properties = list(Property.objects.filter(landlord=landlord))
+
+    if request.method == 'POST':
+        form = LandlordProfileForm(request.POST)
+        if form.is_valid() and form.has_changed():
+            contact.name = form.cleaned_data['name']
+            contact.email = form.cleaned_data['email']
+            contact.address_line1 = form.cleaned_data['address_line1']
+            contact.address_line2 = form.cleaned_data['address_line2']
+            contact.address_line3 = form.cleaned_data['address_line3']
+            contact.home_phone = form.cleaned_data['home_phone']
+            contact.work_phone = form.cleaned_data['work_phone']
+            contact.mobile_phone = form.cleaned_data['mobile_phone']
+            landlord.tenancy_services_id = form.cleaned_data['tenancy_services_id']
+            landlord.is_first_timer = form.cleaned_data['is_first_timer']
+            contact.save()
+            landlord.save()
+    else:
+        form = LandlordProfileForm(initial=initial_landlord_data)
+
+    context = {'form': form, 'properties': properties}
+    return render(request, 'landlord_profile.html', context)
+
+
+def property_manager_profile(request, property_manager_id):
+    property_manager = PropertyManager.objects.get(id=property_manager_id)
+    contact = Contact.objects.get(property_manager=property_manager)
+    initial_property_manager_data = {'name': contact.name,
+                    'email': contact.email,
+                    'address_line1': contact.address_line1,
+                    'address_line2': contact.address_line2,
+                    'address_line3': contact.address_line3,
+                    'home_phone': contact.home_phone,
+                    'work_phone': contact.work_phone,
+                    'mobile_phone': contact.mobile_phone,
+                    }
+
+    properties = list(Property.objects.filter(property_manager=property_manager))
+
+    if request.method == 'POST':
+        form = PropertyManagerProfileForm(request.POST)
+        if form.is_valid() and form.has_changed():
+            contact.name = form.cleaned_data['name']
+            contact.email = form.cleaned_data['email']
+            contact.address_line1 = form.cleaned_data['address_line1']
+            contact.address_line2 = form.cleaned_data['address_line2']
+            contact.address_line3 = form.cleaned_data['address_line3']
+            contact.home_phone = form.cleaned_data['home_phone']
+            contact.work_phone = form.cleaned_data['work_phone']
+            contact.mobile_phone = form.cleaned_data['mobile_phone']
+            contact.save()
+            # Statement below is redundant right now, because there
+            # actually is nothing to save for the PropertyManager
+            # specifically in the current code.
+            property_manager.save()
+    else:
+        form = PropertyManagerProfileForm(initial=initial_property_manager_data)
+
+    context = {'form': form, 'properties': properties}
+    return render(request, 'property_manager_profile.html', context)
