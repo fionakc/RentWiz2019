a027b4dcd536604f7ef4c9718eae33fa275230bc
Danielle Trewoon <trewoodani@AD.ECS.VUW.AC.NZ>
2019-02-12 14:39:29 +1300

    khjk

--- home/views.py
+++ home/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import get_object_or_404, redirect, render
 # Create your views here.
 
 def index(request):
-    landlord_id = 42
-    context = {'landlord_id': landlord_id}
-    return render(request, 'index.html', context)
+    # landlord_id = 42
+    # context = {'landlord_id': landlord_id}
+    return render(request, 'index.html')
